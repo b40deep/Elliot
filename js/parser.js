7b4c33efce74b84ad918dd09bb961b98b72795c2
b40deep <boydtwine@gmail.com>
2023-08-10 17:45:59 +0300

    finished the parser

--- js/parser.js
+++ js/parser.js
@@ -2,17 +2,41 @@
 const textInput = document.getElementById("text-input");
 const textOutput = document.getElementById("text-output");
 const btnParse = document.getElementById("btn-parse");
-btnParse.addEventListener("click", parseEntries);
+btnParse.addEventListener("click", parseEntries2);
 
 const regex = /^([A-Za-z]+ \d+[A-Za-z]*):\n- .+/gm;
 let matches;
 // let text = text;
 
 function parseEntries() {
-  //read the input text
-  //parse it
-  //output it
-  matches = textInput.value.match(regex);
-  console.log(matches);
-  // textOutput.innerHTML = matches;
+	//read the input text
+	//parse it
+	//output it
+	matches = textInput.value.match(regex);
+	console.log(matches);
+	textOutput.innerHTML = matches;
+
+	// for each match (date)
+	// parse for each entry
+	// isolate it as a single array entry
+	// and display them under the date
+	matches.forEach((date) => {
+		entries = date.match(/^(:\n- .+)/gm);
+		console.log(`entries ${entries}`);
+	});
+}
+
+function parseEntries2() {
+	const pattern = /^([^\n:]+):\n((?:- .+\n)*)/gm;
+
+	const matches = [...textInput.value.matchAll(pattern)];
+
+	for (const match of matches) {
+		const heading = match[1];
+		const listItems = match[2].trim().split("\n- ");
+		console.log(`Heading: ${heading}`);
+		for (const item of listItems) {
+			console.log(`${item}`);
+		}
+	}
 }
